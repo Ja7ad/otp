70bfc950c35fe499e7dc12fba000755d6e193a95
Javad <ja7ad@live.com>
2025-04-03 19:51:02 +0330

    feat: validate: Support Digits type for OTP validation

--- validate.go
+++ validate.go
@@ -4,18 +4,20 @@ import (
 	"crypto/subtle"
 )
 
-func validateOTP(code string, secret []byte, counter uint64, digits int, algo Algorithm) (bool, error) {
-	if len(code) != digits {
+func validateOTP(code string, secret []byte, counter uint64, digits Digits, algo Algorithm) (bool, error) {
+	digitInt := digits.Int()
+
+	if len(code) != digitInt {
 		return false, ErrInvalidCodeLength
 	}
 
-	expected, err := deriveOTP(secret, counter, digits, algo)
+	excepted, err := deriveOTP(secret, counter, digitInt, algo)
 	if err != nil {
 		return false, err
 	}
 
 	// Constant-time comparison using unsafe string→[]byte conversion
-	if subtle.ConstantTimeCompare([]byte(code), []byte(expected)) == 1 {
+	if subtle.ConstantTimeCompare([]byte(code), []byte(excepted)) == 1 {
 		return true, nil
 	}
 
